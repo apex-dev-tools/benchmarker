689f549e933689e8f68e013134dc009ee6cd9e41
Peter Wright <pwright@certinia.com>
2025-08-01 11:43:30 +0100

    feat: begin esm migration and update to latest packages

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+import eslint from '@eslint/js';
+import tseslint from 'typescript-eslint';
+
+export default tseslint.config(
+  {
+    // global ignore
+    ignores: ['dist/', 'docs/', 'scripts/', 'eslint.config.js'],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommendedTypeChecked,
+  {
+    languageOptions: {
+      parserOptions: {
+        projectService: true,
+        tsconfigRootDir: import.meta.dirname,
+      },
+    },
+    rules: {
+      quotes: ['warn', 'single', { avoidEscape: true }],
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-require-imports': 'off',
+      '@typescript-eslint/no-unsafe-assignment': 'off',
+      '@typescript-eslint/no-unused-expressions': 'off',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-use-before-define': 'off',
+      '@typescript-eslint/no-var-requires': 'off',
+    },
+  }
+);
