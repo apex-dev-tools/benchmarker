f21c79cd29da40dadf2951da1aa0e52079b5ce49
Peter Wright <pwright@certinia.com>
2025-08-07 16:40:06 +0100

    feat: add initial default module exports with some legacy compat

--- src/index.ts
+++ src/index.ts
@@ -5,14 +5,147 @@
  * @module @apexdevtools/benchmarker
  */
 
-import { ApexBenchmarkService } from "./service/apex.js";
+export {
+  AnonApexBenchmark,
+  type AnonApexBenchmarkFactory,
+  type AnonApexBenchmarkResult,
+  type AnonApexTransaction,
+} from "./benchmark/anon.js";
+export {
+  Benchmark,
+  type BenchmarkId,
+  type BenchmarkResult,
+  type ErrorResult,
+} from "./benchmark/base.js";
+export type { LimitsBenchmarkOptions } from "./benchmark/limits.js";
+export type {
+  GovernorLimits,
+  LimitsContext,
+} from "./benchmark/limits/schemas.js";
 
-export const apexService: ApexBenchmarkService = new ApexBenchmarkService();
+export type { PostgresOptions } from "./database/postgres.js";
+
+export type {
+  LimitsMetric,
+  LimitsMetricProviderOptions,
+  LimitsThresholds,
+} from "./metrics/limits.js";
+export type { Degradation } from "./metrics/limits/deg.js";
+
+export { ApexScriptError } from "./parser/apex/error.js";
+export { ApexScript } from "./parser/apex/script.js";
+export type { NamedSchema } from "./parser/json.js";
+
+export type { ExecuteAnonymousOptions } from "./salesforce/execute.js";
+export { BenchmarkOrg, type BenchmarkOrgOptions } from "./salesforce/org.js";
+export {
+  BenchmarkOrgConnection,
+  connectToSalesforceOrg,
+  type OrgAuthOptions,
+} from "./salesforce/org/connection.js";
+export type {
+  OrgContext,
+  OrgPackage,
+  OrgRelease,
+} from "./salesforce/org/context.js";
+export {
+  DebugLogCategory,
+  DebugLogCategoryLevel,
+  type DebugLogInfo,
+} from "./salesforce/soap/debug.js";
+
+export {
+  ApexBenchmarkService,
+  type ApexBenchmarkServiceOptions,
+  type LimitsBenchmarker,
+  type LimitsBenchmarkRequest,
+  type LimitsBenchmarkResult,
+  type LimitsBenchmarkRun,
+  type LimitsMetrics,
+} from "./service/apex.js";
+export {
+  AnonApexBenchmarker,
+  type AnonApexBenchmarkerRequest,
+  type AnonApexBenchmarkRun,
+} from "./service/apex/runner.js";
+
+export {
+  RunContext,
+  type GlobalOptions,
+  type RunContextOptions,
+} from "./state/context.js";
+export { RunStore } from "./state/store.js";
 
 /*
- * Legacy API for backward compatibility
+ * Partial legacy API for backward compatibility
  */
-export type { BenchmarkOrgConnection as SalesforceConnection } from "./salesforce/org/connection.js";
+
+import { connectToSalesforceOrg as newConnect } from "./salesforce/org/connection.js";
+import { executeAnonymous as executeAnonymousNew } from "./salesforce/execute.js";
+import type { ExecuteAnonymousResult } from "@jsforce/jsforce-node/lib/api/tooling.js";
+import { Connection } from "@salesforce/core";
+
+import type { BenchmarkOrgConnection as SalesforceConnection } from "./salesforce/org/connection.js";
+export type { SalesforceConnection };
+export interface SalesforceAuthInfo {
+  username: string;
+  password?: string;
+  loginUrl?: string;
+  isSFDX?: boolean;
+  version?: string;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace SFConnection {
+  /**
+   * @deprecated This namespace will be removed in a future version.
+   *
+   * Use:
+   * ```
+   * import { connectToSalesforceOrg } from "@apexdevtools/benchmarker";
+   * await connectToSalesforceOrg({...});
+   * ```
+   */
+  export const connectToSalesforceOrg = (
+    authInfoWrapper: SalesforceAuthInfo
+  ): Promise<SalesforceConnection> => {
+    return newConnect(authInfoWrapper);
+  };
+}
+
+// eslint-disable-next-line @typescript-eslint/no-namespace
+export namespace SFUtils {
+  /**
+   * @deprecated This namespace will be removed in a future version.
+   *
+   * Use:
+   * ```
+   * // Replaces namespaces if set by environment or apex.setup() org options
+   * import { ApexBenchmarkService } from "@apexdevtools/benchmarker";
+   * const apex = ApexBenchmarkService.default;
+   * await apex.execute(code, options);
+   *
+   * // Alternatively, for raw execute with string as is
+   * import { executeAnonymous } from "@apexdevtools/benchmarker/salesforce/execute";
+   * await executeAnonymous(connection, code, options);
+   * ```
+   */
+  export const executeAnonymous = (
+    connection: Connection,
+    apexCode: string
+  ): Promise<ExecuteAnonymousResult> => {
+    // TODO ts-retry-promise if not a BenchmarkOrgConnection
+    return executeAnonymousNew(
+      connection,
+      (
+        process.env.BENCH_ORG_UNMANAGED_NAMESPACES?.split(",").map(
+          e => new RegExp(e + "(__|.)", "g")
+        ) || []
+      ).reduce<string>((text, regex) => text.replace(regex, ""), apexCode)
+    );
+  };
+}
+
 export { saveResults } from "./testTemplates/saveResult.js";
 export {
   createApexExecutionTestStepFlow,
