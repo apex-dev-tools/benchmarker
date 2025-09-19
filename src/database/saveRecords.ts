c59eb6249b59186378479af80e3272e4961acb4f
Milan Makwana <mmakwana@certinia.com>
2025-09-19 11:25:44 +0100

    Rename parameter in saveRecords function

--- src/database/saveRecords.ts
+++ src/database/saveRecords.ts
@@ -4,7 +4,7 @@
  */
 import { getConnection } from './connection';
 
-export async function saveRecords<T>(rows: T[]): Promise<T[]> {
+export async function saveRecords<T>(records: T[]): Promise<T[]> {
   const connection = await getConnection();
-  return connection.manager.save(rows);
+  return connection.manager.save(records);
 }
